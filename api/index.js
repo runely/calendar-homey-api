51f8b15c16fa1db2b2c44f2d3e579a45158c76b1
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-11-21 20:11:26 +0100

    Update

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const app = require('express')()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Check out the readme')
+  res.send('Check out the readme please')
 })
 
 app.post('/update', authenticateToken, async (req, res) => {
